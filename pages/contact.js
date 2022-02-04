9baec1c7633f9957869752ba735e1c6a789ae182
JacobRochford <jrochford357@gmail.com>
2022-02-04 11:17:06 -0800

    add contact page

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm/ContactForm';
+import Header from '../components/Header/Header';
+import Section from '../components/Section/Section';
+
+export default function ContactPage() {
+  return <div>
+    <Section>
+      <ContactForm />
+
+    </Section>
+  </div>;
+}
