6d059112e79b7649c8b7f3e03d0965072ed8b369
JacobRochford <jrochford357@gmail.com>
2022-02-04 11:19:04 -0800

    add site themes -colors -fonts -media query breakpoints

---
+++ theme/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    
+  },
+
+  fonts: {
+    title: "courgette",
+    main: "kalam",
+  },
+
+  breakpoints: {
+    sm: "screen and (max-width: 640px)",
+    md: "screen and (min-width: 641px) and (max-width: 768px)",
+    lg: "screen and (min-width: 769px) and (max-width: 1024px)",
+    xl: "screen and (min-width: 1025px) and (max-width: 1280px)",
+  },
+};
+
+export default theme;
