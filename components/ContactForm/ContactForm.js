53425cc62c9444a583bed635b055a469e60c7150
JacobRochford <jrochford357@gmail.com>
2022-02-05 17:17:45 -0800

    add route to homepage after on form submission

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import firebase from '../../FirebaseConfig';
 import { StyledForm } from './ContactForm.styled';
-
+import Router from 'next/router';
 
 const ContactForm = () => {
   const [name, setName] = useState('')
@@ -18,7 +18,7 @@ const ContactForm = () => {
         message: message
       }).then(() => {
         alert('Message Sent!')
-      })
+      }).catch(err => console.log(err.message));
   
       setName('')
       setEmail("");
@@ -28,7 +28,7 @@ const ContactForm = () => {
   }
   
   return (
-    <StyledForm onSubmit={(e) => e.preventDefault()}>
+    <StyledForm onSubmit={e => e.preventDefault()}>
       <h1>Contact Me</h1>
       <input
         name="name"
@@ -54,7 +54,12 @@ const ContactForm = () => {
         onChange={(e) => setMessage(e.target.value)}
         required
       />
-      <button onClick={() => saveContactInfo(name, email, message)}>
+      <button type='submit' onClick={(e) => {
+        e.preventDefault();
+        saveContactInfo(name, email, message);
+        Router.push('/')
+      }
+      }>
         Submit
       </button>
     </StyledForm>
